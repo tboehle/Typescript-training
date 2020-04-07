296a375839f1b89a31b89c828b8d3df062dff22d
Thorben BOEHLE <thorben.boehle@piterion.com>
2020-04-07 17:59:27 +0200

    Integration of tsoa and swaggerUI

--- todo-server/src/app.ts
+++ todo-server/src/app.ts
@@ -1,8 +1,12 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
+
 import { requestLoggerMiddleware }  from './request.logger.middleware';
-import { todoRoutes } from './todo.controller';
+import './todo.controller';
+
+import { RegisterRoutes} from './routes';
+import * as swaggerUi from 'swagger-ui-express';
 
 const app = express();
 app.use(cors());
@@ -11,6 +15,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(requestLoggerMiddleware);
-app.use(todoRoutes);
+RegisterRoutes(app);
+
+try {
+    const swaggerDocument = require('./docs/swagger.json');
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} catch (err) {
+    console.error('Unable to read swagger docs', err);
+}
 
 export { app };
