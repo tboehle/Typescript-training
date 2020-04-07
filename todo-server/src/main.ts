07b2a5eea6662efea2941411b72ecfa72330a221
Thorben BOEHLE <thorben.boehle@piterion.com>
2020-04-07 16:43:22 +0200

    Add mongoose integration

--- todo-server/src/main.ts
+++ todo-server/src/main.ts
@@ -1,17 +1,20 @@
 import { app } from './app';
 import * as http from 'http';
-import { MongoHelper } from './mongo.helper';
+import * as mongoose from 'mongoose';
 
 const PORT = 8080;
-const server = http.createServer(app);
+const MONGO_URI = 'mongodb://localhost:27017';
 
+const server = http.createServer(app);
 server.listen(PORT);
 server.on('listening', async ()=>{
     console.info(`Listening on port ${PORT}`);
-    try {
-        await MongoHelper.connect('mongodb://localhost:27017');
+    mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
         console.info('Connected to Mongo.');
-    } catch (err) {
+    }).catch( err => {
         console.error(err);
-    }
-})
+    });
+});
